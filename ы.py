e6e1c04d7cdeb22b59a7933455b000b3e98d0608
k-e-n-p-a-c-h-i <suedov18@yandex.ru>
2022-01-05 20:39:44 +0300

    ы добавил

---
+++ ы.py
@@ -0,0 +1,32 @@
+import sys
+import pygame
+from buttons import Button
+
+
+pygame.init()
+screen = pygame.display.set_mode((500, 500))
+pygame.display.set_caption('Gui Menu')
+clock = pygame.time.Clock()
+gui_font = pygame.font.Font(None, 30)
+buttons = []
+button1 = Button(buttons, 'Rome', 200, 40, (100, 200), 4, gui_font)
+button2 = Button(buttons, 'Milan', 200, 40, (100, 250), 4, gui_font)
+button3 = Button(buttons, 'Neaples', 200, 40, (100, 300), 4, gui_font, function=lambda: print('hello'))
+
+
+def buttons_draw():
+    for b in buttons:
+        b.update(screen)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill('#DCDDD8')
+    buttons_draw()
+
+    pygame.display.update()
+    clock.tick(60)
