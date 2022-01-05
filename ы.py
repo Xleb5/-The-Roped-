bdbb3f8d483e19d23c58d48bb49c2d3701bf6d2d
k-e-n-p-a-c-h-i <suedov18@yandex.ru>
2022-01-05 21:08:11 +0300

    mistral

--- ы.py
+++ ы.py
@@ -7,11 +7,11 @@ pygame.init()
 screen = pygame.display.set_mode((500, 500))
 pygame.display.set_caption('Gui Menu')
 clock = pygame.time.Clock()
-gui_font = pygame.font.Font(None, 30)
+gui_font = pygame.font.SysFont("Mistral", 30)
 buttons = []
-button1 = Button(buttons, 'Rome', 200, 40, (100, 200), 4, gui_font)
-button2 = Button(buttons, 'Milan', 200, 40, (100, 250), 4, gui_font)
-button3 = Button(buttons, 'Neaples', 200, 40, (100, 300), 4, gui_font, function=lambda: print('hello'))
+button1 = Button(buttons, 'старт', 200, 40, (100, 200), 4, gui_font)
+button2 = Button(buttons, 'магазин', 200, 40, (100, 250), 4, gui_font)
+button3 = Button(buttons, 'выход', 200, 40, (100, 300), 4, gui_font, function=lambda: print('hello'))
 
 
 def buttons_draw():
